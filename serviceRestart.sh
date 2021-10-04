45f8c006a017bf9e593e68e5d9685813d10dfb0d
Poonam Agarwal <agrawal.poonam2015@gmail.com>
2021-10-04 17:47:19 +0530

    handling service name to be specified in command

--- serviceRestart.sh
+++ serviceRestart.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 service_name=$1
-sudo systemctl status $service_name
-status=$?
-echo $status
-if [ $status -eq 0 ]; then
-	echo "service is already running"
-elif [ $status -eq 4 ]; then
-	echo "service doesnot exist"
-elif [ $status -eq 3 ]; then
-	echo "starting service $service_name"
-	`sudo systemctl  restart $service_name`
+if [ "$1" == '' ];then
+	echo 'enter service name to be restarted e.g. bash serviceRestart.sh  docker'
+	exit 1
+else
+	sudo systemctl status $service_name
+	status=$?
+	echo $status
+	if [ $status -eq 0 ]; then
+		echo "service is already running"
+	elif [ $status -eq 4 ]; then
+		echo "service doesnot exist"
+	elif [ $status -eq 3 ]; then
+		echo "starting service $service_name"
+		`sudo systemctl  restart $service_name`
+	fi
 fi
